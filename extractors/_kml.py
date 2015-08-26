e9312f0768caddfbe52437412aecb90744830969
Herb Lainchbury <herb@dynamic-solutions.com>
2015-08-26 08:31:52 -0700

    first working version of kml extractor

--- extractors/_kml.py
+++ extractors/_kml.py
@@ -1,7 +1,26 @@
 """ kml extractor
 """
 
+import tempfile, StringIO
+from osgeo import ogr
 from .. import Extractor
+import json
+import ogr2ogr
+import geojson
+
+def kmltogeojson(data):
+    out = StringIO.StringIO()
+
+    temp = tempfile.NamedTemporaryFile()
+    temp.write(data.read())
+    temp.flush()
+
+    outname = temp.name + '.geojson'
+
+    result = ogr2ogr.main(["", "-f", "GeoJSON", outname, temp.name])
+    if result == True:
+        return open(outname).read()
+
 
 class KmlExtractor(Extractor):
 
@@ -10,5 +29,9 @@ class KmlExtractor(Extractor):
         return path.endswith('.kml') or path.endswith('.kmz')
 
     def extract(self, target, data):
-        return [('kml', 'here is the data!')]
-
+        s = kmltogeojson(data)
+        if s:
+            data = geojson.loads(s)
+            result = geojson.is_valid(data)
+            if result['valid'] == 'yes':
+                return [('geojson', data)]
